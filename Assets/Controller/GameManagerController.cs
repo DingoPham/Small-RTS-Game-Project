55a8eb3851982d70d2b989cc3ed3f1f924c2a2b6
DingoPham <pencibambo@gmail.com>
2025-02-19 13:46:26 +0700

    test

--- Assets/Controller/GameManagerController.cs
+++ Assets/Controller/GameManagerController.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Assets.Controller
+public class GameManagerController : MonoBehaviour
 {
-    internal class GameManagerController
+    public GameObject unitPrefab;
+    public int totalUnits = 10;
+    private List<UnitController> units = new List<UnitController>();
+
+    void Start()
     {
+        SpawnUnits();
+    }
+
+    void SpawnUnits()
+    {
+        for (int i = 0; i < totalUnits; i++)
+        {
+            CreateUnit(i % 2 == 0 ? UnitController.Gender.Male : UnitController.Gender.Female);
+        }
+    }
+
+    public void CreateUnit(UnitController.Gender gender)
+    {
+        GameObject newUnit = Instantiate(unitPrefab, GetRandomPosition(), Quaternion.identity);
+        UnitController controller = newUnit.GetComponent<UnitController>();
+        controller.gender = gender;
+        units.Add(controller);
+
+        // Thay đổi màu theo giới tính
+        SpriteRenderer spriteRenderer = newUnit.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = gender == UnitController.Gender.Male ? Color.blue : Color.red;
+    }
+
+    Vector3 GetRandomPosition()
+    {
+        return new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0);
     }
 }
---
+++ Assets/Functions/LoveSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LoveSystem : MonoBehaviour
+{
+    public float loveRange = 1.5f;  // Khoảng cách yêu nhau
+
+    void Update()
+    {
+        List<UnitController> units = new List<UnitController>(FindObjectsOfType<UnitController>());
+
+        foreach (UnitController unit in units)
+        {
+            foreach (UnitController other in units)
+            {
+                if (unit != other && Vector3.Distance(unit.transform.position, other.transform.position) < loveRange)
+                {
+                    unit.CheckForLove(other);
+                }
+            }
+        }
+    }
+}
